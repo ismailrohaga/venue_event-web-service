63251cccc1b3cc1f4d92df095de1c2a2d2ec8b1f
Ismail Rohaga <azh.zhafir@gmail.com>
2024-04-09 11:22:44 +0700

    feat: organize project structure. move express project to 'nodejs', add sample Go code to 'go'

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const expressConfig = require("./express");
 const serverConfig = require("./server");
+require("dotenv").config();
 
 expressConfig(app, express);
 
