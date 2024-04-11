124c344e9114baca3bb3860478890a4af4301202
Ismail Rohaga <azh.zhafir@gmail.com>
2024-04-11 18:28:30 +0700

    feat: add complete implementation of auth route test

--- nodejs/src/test/auth.test.js
+++ nodejs/src/test/auth.test.js
@@ -1,10 +1,44 @@
 const request = require("supertest");
+const { faker } = require("@faker-js/faker");
 const app = require("../../app");
+
+// must research on mocking or seperate test env
 
 const loginCredential = {
   email: "azh.zhafir@gmail.com",
   password: "password",
 };
+
+//register
+describe("GET /api/auth/register", () => {
+  let token;
+  let email;
+  let name;
+  beforeAll(async () => {
+    email = faker.internet.email();
+    name = faker.person.firstName();
+  });
+  afterAll(async () => {
+    await request(app)
+      .post("/api/auth/logout")
+      .set("Authorization", `Bearer ${token}`);
+  });
+  test("should return 200", async () => {
+    const response = await request(app).post("/api/auth/register").send({
+      email: email,
+      name: name,
+      role: "USER",
+      password: "password",
+    });
+    token = response.body.accessToken;
+    expect(response.statusCode).toBe(200);
+    expect(response.body.user.email).toBe(email);
+  });
+  test("should return 500", async () => {
+    const response = await request(app).post("/api/auth/register");
+    expect(response.statusCode).toBe(500);
+  });
+});
 
 //login
 describe("GET /api/auth/login", () => {
@@ -22,6 +56,10 @@ describe("GET /api/auth/login", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.user.email).toBe("azh.zhafir@gmail.com");
   });
+  test("should return 500", async () => {
+    const response = await request(app).post("/api/auth/login");
+    expect(response.statusCode).toBe(500);
+  });
 });
 
 // get profile
@@ -35,7 +73,7 @@ describe("GET /api/auth/profile", () => {
   });
   afterAll(async () => {
     await request(app)
-      .post("/auth/logout")
+      .post("/api/auth/logout")
       .set("Authorization", `Bearer ${token}`);
   });
   test("should return 200", async () => {
@@ -43,11 +81,12 @@ describe("GET /api/auth/profile", () => {
       .get("/api/auth/profile")
       .set("Authorization", `Bearer ${token}`);
     expect(response.statusCode).toBe(200);
+    expect(response.body.user.email).toBe("azh.zhafir@gmail.com");
   });
-  test("should return user", async () => {
+  test("should return 401", async () => {
     const response = await request(app)
       .get("/api/auth/profile")
-      .set("Authorization", `Bearer ${token}`);
-    expect(response.body.user.email).toBe("azh.zhafir@gmail.com");
+      .set("Authorization", `Bearer eyJhbGci`);
+    expect(response.statusCode).toBe(401);
   });
 });
